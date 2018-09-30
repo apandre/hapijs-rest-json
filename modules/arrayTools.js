8a940fa152a9f5ba0770d5f623e17c2af0af1073
Alex Pandre <apandre@gmail.com>
2018-09-30 09:17:53 -0400

    Add modules/ dir

---
+++ modules/arrayTools.js
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+
+/**
+ * @summary remove element from array
+ */
+var removeArrayElement = function ( array, element ) {
+	const index = array.indexOf(element);
+	
+	if (index !== -1) {
+		array.splice(index, 1);
+	}
+	return array;
+}
+
+
+module.exports = {
+	removeArrayElement
+};
